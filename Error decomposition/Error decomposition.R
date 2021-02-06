045813c1819ea262d4aafaac6e408d2f6db973d7
checcomi <checcomi@gmail.com>
2021-02-06 09:17:17 +0000

    separate code for the example

--- Error decomposition/Error decomposition.R
+++ Error decomposition/Error decomposition.R
@@ -36,23 +36,3 @@ MSEdec <- function(var,obs,sim,method){
   }
 }
 
-read.csv("outdata0.csv")
-resultV1<- MSEdec("V",allData0[which(allData0$var=="V")]$obs,allData0[which(allData0$var=="V")]$sim,method = 1)
-resultV2<- MSEdec("V",allData0[which(allData0$var=="V")]$obs,allData0[which(allData0$var=="V")]$sim,method = 2)
-resultB1<- MSEdec("B",allData0[which(allData0$var=="B")]$obs,allData0[which(allData0$var=="B")]$sim,method = 1)
-resultB2<- MSEdec("B",allData0[which(allData0$var=="B")]$obs,allData0[which(allData0$var=="B")]$sim,method = 2)
-resultH1<- MSEdec("H",allData0[which(allData0$var=="H")]$obs,allData0[which(allData0$var=="H")]$sim,method = 1)
-resultH2<- MSEdec("H",allData0[which(allData0$var=="H")]$obs,allData0[which(allData0$var=="H")]$sim,method = 2)
-resultHc1<- MSEdec("Hc",allData0[which(allData0$var=="Hc")]$obs,allData0[which(allData0$var=="Hc")]$sim,method = 1)
-resultHc2<- MSEdec("Hc",allData0[which(allData0$var=="Hc")]$obs,allData0[which(allData0$var=="Hc")]$sim,method = 2)
-resultD1<- MSEdec("D",allData0[which(allData0$var=="D")]$obs,allData0[which(allData0$var=="D")]$sim,method = 1)
-resultD2<- MSEdec("D",allData0[which(allData0$var=="D")]$obs,allData0[which(allData0$var=="D")]$sim,method = 2)
-resultN1<- MSEdec("N",allData0[which(allData0$var=="N")]$obs,allData0[which(allData0$var=="N")]$sim,method = 1)
-resultN2<- MSEdec("N",allData0[which(allData0$var=="N")]$obs,allData0[which(allData0$var=="N")]$sim,method = 2)
-
-method_Korb <- Map(c,resultV1,resultB1,resultH1,resultHc1,resultD1,resultN1)
-method_Korb <- data.frame(matrix(unlist(method_Korb), nrow=length(method_Korb), byrow=T))
-row.names(method_Korb)<-c("Var","sb","sdsd","lc","mse")
-method_Gauchal <- Map(c,resultV2,resultB2,resultH2,resultHc2,resultD2,resultN2)
-method_Gauchal <- data.frame(matrix(unlist(method_Gauchal), nrow=length(method_Gauchal), byrow=T))
-row.names(method_Gauchal)<-c("Var","sb","nu","lc","mse")
